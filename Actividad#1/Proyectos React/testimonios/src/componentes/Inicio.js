ebc3454db22ad02800f045f122338aacc006d30c
carloscastano25 <carlos.castano@est.iudigital.edu.co>
2022-12-07 20:39:19 -0500

    React

---
+++ Actividad#1/Proyectos React/testimonios/src/componentes/Inicio.js
@@ -0,0 +1,17 @@
+import react from 'react';
+
+function Inicio ()
+{
+    return
+    (
+        <div className = 'contenedor-inicio'>
+            <img className = 'contenedor-inicio' src=''/>
+            <h1>Página de restaurantes</h1>
+        </div>
+
+
+    )
+
+
+}
+export default Inicio;
